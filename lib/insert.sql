08ef8bd20b63a6da3140238d9a85330635832560
Mickey <micksheridan.24@gmail.com>
2019-02-23 17:06:13 -0500

    submit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) values ("Mr. Chocolate", 12,"Male", "Brown", "Moody", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) values ("Rowdy", 12, "Male","Brown", "Hungry", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) values ("Tabitha", 8, "Female","Black", "Happy", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) values ("Sergeant Brown", 4,"Male", "Brown", "Joyous", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) values ("Melissa", 5, "Female","Brown","Bipolar", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) values ("Grinch", 4,"Male", "Green", "Resentful", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) values ("Wendy", 2,"Female", "Black",  "Livid", "true");
+INSERT INTO bears(name, age, gender, color, temperament, alive) values (NULL, 102,"Wicked", "Bronze", "Evil", "true");
